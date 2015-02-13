b685a581d05dfb3ede7bb6c30ffe94f63f483915
Göran Svensson <goran@molnfront.com>
2015-02-13 20:20:11 +0100

    sdsdds

--- test/js/moltin.js
+++ test/js/moltin.js
@@ -178,7 +178,7 @@ Moltin = (function() {
       },
       async: typeof callback === 'function' ? true : false,
       headers: {
-       // 'Content-Type': 'application/x-www-form-urlencoded'
+       'Content-Type': 'application/x-www-form-urlencoded'
         //'Content-Type': 'multipart/form-data'
       },
       success: (function(_this) {
